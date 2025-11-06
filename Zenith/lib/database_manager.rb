984a1acc322f450fbbc1080761c5d430ebd86ec5
krnlz | emilejroy <emilejoncas.roy@gmail.com>
2025-11-06 17:58:31 -0500

    init
    Creating the default files + the database for the project

---
+++ Zenith/lib/database_manager.rb
@@ -0,0 +1,34 @@
+require 'sqlite3'
+require 'bcrypt'
+
+module DatabaseManager
+
+  # Openning the database
+  DB = SQLite3::Database.new 'task_manager.db'
+
+  def self.setup_database
+    puts "Database setup"
+    # Creating the table 'users'
+    DB.execute <<-SQL
+    create table if not exists users (
+        id integer primary key autoincrement,
+        username varchar(30) not null,
+        password_digest text not null
+    );
+    SQL
+
+    #Creating the table 'tasks'
+    DB.execute <<-SQL
+    create table if not exists tasks (
+        id integer primary key autoincrement,
+        subject text not null,
+        user_id integer not null,
+        description text not null,
+        completed boolean not null,
+        foreign key (user_id) references users(id)
+    );
+    SQL
+
+    puts "Database ready"
+  end
+end
